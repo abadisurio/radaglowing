0c1beca7942dc09286656aaf837fa8ab5c5a8185
Abadi Suryo <abadisuryosetiyo@gmail.com>
2022-06-11 15:52:57 +0700

    radaglowing: fix grid on featured

--- src/page/Featured.js
+++ src/page/Featured.js
@@ -116,7 +116,7 @@ const FeaturedPage = () => {
                 <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
                     <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Customers also purchased</h2>
 
-                    <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-4 lg:grid-cols-6 xl:gap-x-8">
+                    <div className="mt-6 grid grid-cols-3 gap-y-10 gap-x-6 sm:grid-cols-4 lg:grid-cols-6 xl:gap-x-8">
                         {isLoading
                             ? <h1>isLoading</h1>
                             : (featuredDetail.productListId.split(", ")).map((productId) => {
