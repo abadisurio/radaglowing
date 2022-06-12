98585ef4b6fed200ce4a1ad64ba517e93b06298f
Abadi Suryo <abadisuryosetiyo@gmail.com>
2022-06-12 10:00:08 +0700

    radaglowing: remove ga tracker from mount

--- src/page/Explore.js
+++ src/page/Explore.js
@@ -1,13 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import ProductCollection from '../component/ProductCollection'
-import useGaTRacker from '../utils/useGaTRacker';
 
 const ExplorePage = () => {
-    const gaEventTracker = useGaTRacker('Explore Page');
-    useEffect(() => {
-        gaEventTracker('explore')
-    })
-
     return (
         <ProductCollection />
     )
