1c6bb9de8baac0caa739b487000da679c26540ec
Abadi Suryo <abadisuryosetiyo@gmail.com>
2022-06-12 21:54:35 +0700

    radaglowing: add alert

--- src/page/ProductPage.js
+++ src/page/ProductPage.js
@@ -28,6 +28,7 @@ const ProductPage = () => {
     const cart = useContext(CartContext)
     const [isLoading, setLoading] = useState(true)
     const [productDetail, setProductDetail] = useState(initialDetail)
+    const [showAlert, setShowAlert] = useState(false)
     // const cart = useCart()
 
     let { id } = useParams();
@@ -45,17 +46,24 @@ const ProductPage = () => {
         return () => { document.title = "Rada Glow" }
     }, [isLoading, id])
 
+    const doShowAlert = () => {
+        setShowAlert(true)
+        return setTimeout(() => {
+            setShowAlert(false)
+        }, 1000)
+    }
 
     const addToCart = (e) => {
         // console.log('e', e)
         console.log('cart', cart)
         cart.addProduct(e.target.name)
+        doShowAlert()
         e.preventDefault()
     }
 
     return (
         <div className="bg-white">
-            <div className="pt-6">
+            <div className="pt-6 ">
                 {isLoading ? <h1>Loading</h1> : (
                     <>
                         {/* Image gallery */}
@@ -125,16 +133,19 @@ const ProductPage = () => {
                                         </span>
                                     </div>
                                 </div>
-
                                 <form className="mt-10">
-
                                     <button
                                         onClick={addToCart}
                                         name={productDetail.id}
-                                        className="mt-10 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                        className=" w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                     >
                                         Tambah ke Keranjang
                                     </button>
+                                    {showAlert && (
+                                        <div className="flex mt-3 justify-center text-white bg-gradient-to-r from-indigo-500 to-pink-500 rounded-md  p-3 ">
+                                            <span>Produk ditambahkan ke keranjang</span>
+                                        </div>
+                                    )}
                                 </form>
                             </div>
 
