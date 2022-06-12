bdc82e97869997c4f16ea7f0e11e2cb130b95f27
Abadi Suryo <abadisuryosetiyo@gmail.com>
2022-06-13 01:07:18 +0700

    radaglowing: add linkedin link

--- src/page/About.js
+++ src/page/About.js
@@ -12,41 +12,45 @@ const About = () => {
             <div className='text-center p-5 self-center'>
                 <h1 className="text-5xl racking-tight font-extrabold text-gray-900 sm:text-5xl md:text-7xl mb-10">
                     <span className="block xl:inline">This is </span>
-                    <span className="block text-indigo-600 xl:inline">Rada Glow </span>
+                    <span className='block xl:inline'>rada<span className="text-indigo-600">Glow </span></span>
                     <span className="block xl:inline">team</span>
                 </h1>
                 {/* <p className="mt-3 text-xl text-gray-500 w-96 text-center mx-auto">
                     Pesananmu akan kami proses
                 </p> */}
                 <div className="grid grid-cols-2 md:grid-cols-4 gap-4 px-5">
-                    <div className="p-5">
+                    <a href="https://linkedin.com/in/abadisurio" rel='noreferrer' target={'_blank'} className="p-5 hover:bg-blue-200 transition duration-150 rounded-lg">
                         <div className="mx-auto inline-block ring-white rounded-full h-32 w-32 overflow-hidden flex justify-center bg-gradient-to-b from-indigo-500 to-violet-300">
                             <img class="object-cover" src={require('../asset/image/Abadi.png')} alt="Foto Abadi" />
                         </div>
                         <h1 className='mt-3 mb-0 text-xl font-bold text-slate-700'>Abadi Suryo</h1>
-                        <h1 className='text-lg font-bold text-slate-400'>Code</h1>
-                    </div>
-                    <div className="p-5">
+                        <h1 className='text-lg font-bold text-slate-500'>Code</h1>
+                        <h1 className='text-lg font-bold text-white'>See LinkedIn</h1>
+                    </a>
+                    <a href="https://www.linkedin.com/in/arosochiyosua/" rel='noreferrer' target={'_blank'} className="p-5 hover:bg-blue-200 transition duration-150 rounded-lg">
                         <div className="mx-auto inline-block ring-white rounded-full h-32 w-32 overflow-hidden flex justify-center bg-gradient-to-b from-orange-500 to-amber-400">
                             <img class="object-cover" src={require('../asset/image/Archi.png')} alt="Foto Arosochi" />
                         </div>
                         <h1 className='mt-3 mb-0 text-xl font-bold text-slate-700'>Arosochi Yosua D.</h1>
-                        <h1 className='text-lg font-bold text-slate-400'>Product</h1>
-                    </div>
-                    <div className="p-5">
+                        <h1 className='text-lg font-bold text-slate-500'>Product</h1>
+                        <h1 className='text-lg font-bold text-white'>See LinkedIn</h1>
+                    </a>
+                    <a href="https://www.linkedin.com/in/daviadi-auzan-fadhlillah-10878a183/" rel='noreferrer' target={'_blank'} className="p-5 hover:bg-blue-200 transition duration-150 rounded-lg">
                         <div className="mx-auto inline-block ring-white rounded-full h-32 w-32 overflow-hidden flex justify-center bg-gradient-to-b from-rose-600 to-fuchsia-500">
                             <img class="object-cover" src={require('../asset/image/Daviadi.png')} alt="Foto Daviadi" />
                         </div>
                         <h1 className='mt-3 mb-0 text-xl font-bold text-slate-700'>Daviadi Auzan F.</h1>
-                        <h1 className='text-lg font-bold text-slate-400'>Analytics</h1>
-                    </div>
-                    <div className="p-5">
+                        <h1 className='text-lg font-bold text-slate-500'>Analytics</h1>
+                        <h1 className='text-lg font-bold text-white'>See LinkedIn</h1>
+                    </a>
+                    <a href="http://www.linkedin.com/in/rahma-nur-annisa-16962214a/" rel='noreferrer' target={'_blank'} className="p-5 hover:bg-blue-200 transition duration-150 rounded-lg">
                         <div className="mx-auto inline-block ring-white rounded-full h-32 w-32 overflow-hidden flex justify-center bg-gradient-to-b from-lime-600 to-yellow-300">
                             <img class="object-cover" src={require('../asset/image/Rahma.png')} alt="Foto Rahma" />
                         </div>
                         <h1 className='mt-3 mb-0 text-xl font-bold text-slate-700'>Rahma Nur Annisa</h1>
-                        <h1 className='text-lg font-bold text-slate-400'>Design</h1>
-                    </div>
+                        <h1 className='text-lg font-bold text-slate-500'>Design</h1>
+                        <h1 className='text-lg font-bold text-white'>See LinkedIn</h1>
+                    </a>
                 </div>
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center">
 
